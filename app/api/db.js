2967d1d6999502a63303c13a56a84c84cfb95262
Yamin Hossain <streatdot616@gmail.com>
2024-03-27 14:09:27 +0600

    ldl

--- app/api/db.js
+++ app/api/db.js
@@ -38,11 +38,7 @@ db.sequelize = sequelize
 // db.products = require('./product/model/model.js')(sequelize, DataTypes)
 
 // user
-db.user_Designations = require('./user/user_designations/model/model.js')(sequelize, DataTypes)
-db.user_infos = require('./user/user_infos/model/model.js')(sequelize, DataTypes)
-db.user_work_departments = require('./user/user_work_departments/model/model.js')(sequelize, DataTypes)
-db.user_work_users = require('./user/user_work_users/model/model.js')(sequelize, DataTypes)
-db.user_works = require('./user/user_works/model/model.js')(sequelize, DataTypes)
+
 db.users = require('./user/users/model/model.js')(sequelize, DataTypes)
 
 
